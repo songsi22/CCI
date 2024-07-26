4f604ff6b91f95ec6cb912f7a2d010b330c284df
rew <songsi22@naver.com>
2024-07-26 11:06:20 +0900

    update template, data_to_excel

--- data_to_excel.py
+++ data_to_excel.py
@@ -7,8 +7,8 @@ def data_to_excel(inventories):
     for i,inventory in  enumerate(inventories):
         ws[f'A{i+3}'].value = inventory['availability_zone']# zone
         ws[f'B{i + 3}'].value = inventory['name']  # name
-        ws[f'O{i+3}'].value = inventory['publicip']# pub ip
-        ws[f'P{i+3}'].value = inventory['privateip']# pri ip
-        ws[f'Q{i+3}'].value = inventory['created']# created
-        ws[f'R{i+3}'].value = inventory['vm_state'] # vm state
+        ws[f'L{i+3}'].value = inventory['publicip']# pub ip
+        ws[f'M{i+3}'].value = inventory['privateip']# pri ip ## 향후 주석 처리 예정?
+        ws[f'N{i+3}'].value = inventory['created']# created
+        ws[f'O{i+3}'].value = inventory['vm_state'] # vm state
     wb.save('./inventory.xlsx')
