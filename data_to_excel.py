01d7f1f034a7f9814dd4a3322d2ca94481f01851
rew <songsi22@naver.com>
2024-08-01 09:36:33 +0900

    add customer variable template.xlsx, data_to_excel

--- data_to_excel.py
+++ data_to_excel.py
@@ -3,7 +3,8 @@ from openpyxl.styles import Border, Side, Alignment
 from datetime import datetime
 import string
 
-def data_to_excel(inventories, csp_type):
+
+def data_to_excel(inventories, csp_type, customer=''):
     thin_border = Border(left=Side(style='thin'),
                          right=Side(style='thin'),
                          top=Side(style='thin'),
@@ -15,16 +16,19 @@ def data_to_excel(inventories, csp_type):
         ext_hdd = 0
         vmguestip = next(iter(inventory))
         inventory = inventory[vmguestip]
-        ws[f'A{i + 4}'].value = inventory['availability_zone']  # zone
-        ws[f'B{i + 4}'].value = inventory['name']  # name
-        ws[f'D{i + 4}'].value = inventory['vcpus']  # vcpu
-        ws[f'E{i + 4}'].value = inventory['ram']  # ram
+        create_time_in_excel = datetime.now().strftime("%Y년%m월%d일")
+        ws['A1'].value = customer
+        ws['O1'].value = create_time_in_excel
+        ws[f'A{i + 5}'].value = inventory['availability_zone']  # zone
+        ws[f'B{i + 5}'].value = inventory['name']  # name
+        ws[f'D{i + 5}'].value = inventory['vcpus']  # vcpu
+        ws[f'E{i + 5}'].value = inventory['ram']  # ram
         for volume in inventory['volumes']:
             if volume['bootable']:
                 if volume['volume_type'] == 'HDD':
-                    ws[f'F{i + 4}'].value = volume['size']  # HDD size
+                    ws[f'F{i + 5}'].value = volume['size']  # HDD size
                 else:
-                    ws[f'G{i + 4}'].value = volume['size']  # SSD size
+                    ws[f'G{i + 5}'].value = volume['size']  # SSD size
             else:
                 if volume['volume_type'] == 'HDD':
                     ext_hdd += volume['size']
@@ -33,15 +37,15 @@ def data_to_excel(inventories, csp_type):
         if ext_ssd == 0 and ext_hdd == 0:
             pass
         elif ext_ssd != 0:
-            ws[f'J{i + 4}'].value = ext_ssd  # ext ssd
+            ws[f'J{i + 5}'].value = ext_ssd  # ext ssd
         else:
-            ws[f'I{i + 4}'].value = ext_hdd  # ext ssd
-        ws[f'M{i + 4}'].value = inventory['publicip']  # pub ip
-        ws[f'N{i + 4}'].value = vmguestip  # pri ip ## 향후 주석 처리 예정?
-        ws[f'O{i + 4}'].value = inventory['created']  # created
-        ws[f'P{i + 4}'].value = inventory['vm_state']  # vm state
+            ws[f'I{i + 5}'].value = ext_hdd  # ext ssd
+        ws[f'M{i + 5}'].value = inventory['publicip']  # pub ip
+        ws[f'N{i + 5}'].value = vmguestip  # pri ip ## 향후 주석 처리 예정?
+        ws[f'O{i + 5}'].value = inventory['created']  # created
+        ws[f'P{i + 5}'].value = inventory['vm_state']  # vm state
         for uppercase in string.ascii_uppercase[:-10]:
-            ws[f'{uppercase}{i + 4}'].border = thin_border
-            ws[f'{uppercase}{i + 4}'].alignment = Alignment(horizontal='center', vertical='center')
-    create_time = datetime.now().strftime("%Y%m%d-%H%M")
-    wb.save(f'./{csp_type}_inventory_{create_time}.xlsx')
+            ws[f'{uppercase}{i + 5}'].border = thin_border
+            ws[f'{uppercase}{i + 5}'].alignment = Alignment(horizontal='center', vertical='center')
+    create_time_in_file = datetime.now().strftime("%Y%m%d-%H%M")
+    wb.save(f'./{customer}{csp_type}_inventory_{create_time_in_file}.xlsx')
