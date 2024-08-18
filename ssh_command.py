f1f4deaee6c0ff25b8121aae4b7e929d2b44ad09
kiuk ryu <60905654+songsi22@users.noreply.github.com>
2024-08-18 16:30:57 +0900

    Update ssh_command.py

--- ssh_command.py
+++ ssh_command.py
@@ -12,10 +12,14 @@ def get_server_info(hostname, user, password, port=22):
         ssh.connect(hostname=hostname, port=port, username=user, password=password)
         shell = ssh.invoke_shell()
         shell.send(
-            "echo START;echo OS: `hostnamectl | grep 'Operating System:' | awk -F': ' '{print $2}' | sed 's/ Linux//' | sed 's/ (.*)//' | sed 's/ LTS//'`;"
-            "echo HOSTNAME: `hostname`;"
-            "echo SWAP: `free -k|grep -i swap|awk '{print $2/1024/1024}'`; echo Mount Point: `grep ^[A-Z,a-z,/] /etc/fstab|egrep -v 'swap|boot' | awk '$2 != \"/\" {print $2}'`;"
-            "echo NAS: `df -k | grep ^[1,2]|awk '{print $2/1024/1024,$6}'`;echo IPs: `hostname -I`;echo END\n")
+            "echo START;"
+            "echo OS:`hostnamectl | grep 'Operating System:' | awk -F': ' '{print $2}' | sed 's/ Linux//' | sed 's/ (.*)//' | sed 's/ LTS//'`;"
+            "echo HOSTNAME:`hostname`;"
+            "echo SWAP:`free -k|grep -i swap|awk '{print int($2/1024/1024+0.5)}'`; echo Mount Point:`grep ^[A-Z,a-z,/] /etc/fstab|egrep -v 'swap|boot' | awk '$2 != \"/\" {print $2}'`;"
+            "echo NAS:`df -k | grep ^[1,2]|awk '{print int($2/1024/1024+0.5),$6}'`;"
+            "echo IPs:`hostname -I`;"
+            "echo END\n")
+
         time.sleep(1.3)  # 명령어가 실행될 시간을 줌
 
         output = shell.recv(4096).decode()
@@ -23,6 +27,7 @@ def get_server_info(hostname, user, password, port=22):
 
         if match:
             data = match.group(1).strip()
+            print(data)
             return parse_system_info(data)
         else:
             return {"error": "No valid data found between START and END markers"}
@@ -40,42 +45,41 @@ def get_server_info(hostname, user, password, port=22):
 
 
 def parse_system_info(data: str):
-    os_name_match = re.search(r'OS:\s*\w+\s+\d+(\.\d+)*', data)
-    os_info = os_name_match.group(0).split('OS: ')[1] if os_name_match else "Unknown"
 
-    hostname_match = re.search(r'HOSTNAME:\s(\S*)', data)
+    os_match = re.search(r'OS:\S+\s+\S+', data)
+    os_info = os_match.group(0).split('OS:')[1] if os_match else "Unknown"
+
+    hostname_match = re.search(r'HOSTNAME:([^\n]+)', data)
     hostname = hostname_match.group(1) if hostname_match else ""
 
     # Swap 정보 추출
-    swap_match = re.search(r'SWAP:\s*(\d+)', data)
+    swap_match = re.search(r'SWAP:([^\n]+)', data)
     swap_size = "" if swap_match and swap_match.group(1) == "0" else swap_match.group(1)
 
-    # import pdb;pdb.set_trace()
-    mp_match = re.search(r'Mount Point:\s\s*(.*)', data)
+    mp_match = re.search(r'Mount Point:*([^\n]+)', data)
     if mp_match:
         mp_list = mp_match.group(1).split() if mp_match else []
     else:
         mp_list = ''
     # NAS 정보 추출
-    nas_match = re.search(r'NAS:\s*(\d+)\s+(/[^ ]+)', data)
-    nas_size = nas_match.group(1) if nas_match else ""
-    nas_point = nas_match.group(2) if nas_match else ""
-
+    nas_match = re.search(r'NAS:*([^\n]+)', data)
+    nas_data = nas_match.group(0).split("NAS:")[1].strip()
+    nas_size = re.findall(r'\d+(?:\.\d+)?', nas_data)
+    nas_point = re.findall(r'/\S+', nas_data)
+    mountpoint = mp_list+nas_point
     # IP 정보 추출
-    ip_match = re.search(r'IPs:\s*(.*)', data)
+    ip_match = re.search(r'IPs:*([^\n]+)', data)
     if ip_match:
         ip_list = ip_match.group(1).split() if ip_match else []
     else:
         ip_list = ''
-
     system_info = {
         ip_list[0]: {
             "OS": os_info,
             "hostname": hostname,
             "swap": swap_size,
-            "MountPoint": mp_list,
+            "MountPoint": mountpoint,
             "NASsize": nas_size,
-            "NASpoint": nas_point,
             "IPs": ip_list
         }
     }
